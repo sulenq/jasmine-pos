8c47e3f48b80d4613097435dd72c945fcfa1d346
sulenq <whyimustwriteit@gmail.com>
2023-08-31 17:47:04 +0700

    add dashboard page

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,9 @@ import "./global.css";
 import { myTheme } from "./myTheme";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
-import SignIn from "./components/SignIn";
-import SignUp from "./components/SignUp";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Dashboard from "./pages/Dashboard";
 
 export const App = () => (
   <ChakraProvider theme={myTheme}>
@@ -13,6 +14,7 @@ export const App = () => (
         <Route path="/" element={<LandingPage />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   </ChakraProvider>
